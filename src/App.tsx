9df9073a0fda696ce8bcaf2d4be22ec5d343522c
petraelgemyr <petra.elgemyr@gmail.com>
2023-12-18 22:52:06 +0100

    uppdatera app.tsx med nya datamodeller

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,13 @@ import { RouterProvider } from "react-router-dom";
 import "./App.css";
 import { router } from "./components/Router";
 import { useState } from "react";
-import { PostEntrySkeleton } from "./components/models/Post";
-import { DogEntrySkeleton } from "./components/models/Dog";
+import { Post } from "./components/models/Post";
+import { Dog } from "./components/models/Dog";
 import { AppContext } from "./components/contexts/AppContext";
 
 function App() {
-  const [posts, setPosts] = useState<PostEntrySkeleton[]>([]);
-  const [dogs, setDogs] = useState<DogEntrySkeleton[]>([]);
+  const [posts, setPosts] = useState<Post[]>([]);
+  const [dogs, setDogs] = useState<Dog[]>([]);
   const [pageId, setPageId] = useState<string>("");
 
   const contextValue = {
