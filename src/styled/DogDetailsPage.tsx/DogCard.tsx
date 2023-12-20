3194a19f13f155f10581dba88087d20ab0d0f943
petraelgemyr <petra.elgemyr@gmail.com>
2023-12-20 14:23:54 +0100

    styling hundkort

--- src/styled/DogDetailsPage.tsx/DogCard.tsx
+++ src/styled/DogDetailsPage.tsx/DogCard.tsx
@@ -17,18 +17,24 @@ export const StyledDiv = styled(Col)`
 
 export const ColContainer = styled(Col)`
   width: 80%;
+
   @media screen and (${devices.tablet}) {
     flex-direction: row;
     justify-content: space-evenly;
     flex-wrap: wrap;
+    width: 95%;
     /* justify-content: flex-start; */
+    padding: 0;
+  }
+  @media screen and (${devices.laptop}) {
+    width: 100%;
   }
 `;
 
 export const DogCard = styled(Col)`
-  justify-content: center;
+  justify-content: flex-start;
   align-items: flex-start;
-  width: 80vw;
+  width: 100%;
   padding: 0;
   margin-top: 10%;
   margin-bottom: 5%;
@@ -40,31 +46,37 @@ export const DogCard = styled(Col)`
   color: white;
 
   @media screen and (${devices.tablet}) {
-    width: 30vw;
-    margin: 3%;
+    width: 30%;
+    margin: 0;
+    margin-top: 5%;
   }
 
   @media screen and (${devices.laptop}) {
-    width: 25vw;
+    width: 27%;
+    margin-top: 2%;
+    margin-bottom: 2%;
+    /* height: max-content; */
   }
 `;
 
 export const ImageContainer = styled.div`
   width: 100%;
-  height: 65%;
+  /* height: 65%; */
   margin: 0;
   padding: 0;
   object-fit: cover;
   object-position: center;
 
   @media screen and (${devices.laptop}) {
-    height: 70%;
+    height: unset;
+  }
+
+  @media screen and (${devices.fourK}) {
   }
 `;
 
 export const Image = styled.img`
   width: 100%;
-  /* height: 100%; */
   border-radius: 10px 10px 0 0;
   margin: 0;
   padding: 0;
@@ -87,6 +99,14 @@ export const CardTextContainer = styled(Col)`
 export const CardText = styled.span`
   display: block;
   font-size: 1rem;
+
+  @media screen and (${devices.tablet}) {
+    font-size: 1.1rem;
+  }
+
+  @media screen and (${devices.laptop}) {
+    font-size: 1.2rem;
+  }
 `;
 
 export const PrimaryButton = styled.button`
@@ -111,6 +131,10 @@ export const ButtonContainer = styled(Row)<{ width?: string }>`
   width: ${({ width }) => (width ? width : "100%")};
   justify-content: space-between;
   margin-top: 5%;
+
+  @media screen and (${devices.tablet}) {
+    width: ${({ width }) => (width ? "90%" : "100%")};
+  }
 `;
 
 export const FilterButton = styled.button`
--- src/styled/devices.ts
+++ src/styled/devices.ts
@@ -1,4 +1,5 @@
 export const devices = {
   tablet: "min-width: 768px",
   laptop: "min-width: 1024px",
+  fourK: "min-width: 2560px",
 };
