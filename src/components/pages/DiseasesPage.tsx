fdc4b449eda2b46670e2dcf8753ad3fccaee00cf
petraelgemyr <petra.elgemyr@gmail.com>
2024-01-06 00:50:02 +0100

    skapa spaniensidor

--- src/components/pages/DiseasesPage.tsx
+++ src/components/pages/DiseasesPage.tsx
@@ -1,7 +1,20 @@
+import { SecondaryButton } from "../../styled/Buttons/PrimaryButton";
+import { useNavigate } from "react-router";
+
 export const DiseasesPage = () => {
+  const navigate = useNavigate();
+
   return (
     <>
       <h2>Sjukdomar</h2>
+
+      <p>Vill du läsa mer om hundarna vi räddar?</p>
+      <SecondaryButton onClick={() => navigate("/situationen-i-spanien")}>
+        Siutationen i Spanien
+      </SecondaryButton>
+      <SecondaryButton onClick={() => navigate("/myter-om-gatuhundar")}>
+        Myter om gatuhundar
+      </SecondaryButton>
     </>
   );
 };
--- src/components/pages/MythPage.tsx
+++ src/components/pages/MythPage.tsx
@@ -1,7 +1,20 @@
+import { useNavigate } from "react-router";
+import { SecondaryButton } from "../../styled/Buttons/PrimaryButton";
+
 export const MythPage = () => {
+  const navigate = useNavigate();
+
   return (
     <>
       <h2>Myter om gatuhundar</h2>
+
+      <p>Vill du läsa mer om hundarna vi räddar?</p>
+      <SecondaryButton onClick={() => navigate("/situationen-i-spanien")}>
+        Siutationen i Spanien
+      </SecondaryButton>
+      <SecondaryButton onClick={() => navigate("/sjukdomar")}>
+        Sjukdomar
+      </SecondaryButton>
     </>
   );
 };
