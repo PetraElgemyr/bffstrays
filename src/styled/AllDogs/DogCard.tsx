b5364cdc3e095cb2ffa19c25d0e4a51a21ed09cf
petraelgemyr <petra.elgemyr@gmail.com>
2024-01-07 01:53:43 +0100

    lägga atill pekhand vid hovering

--- src/styled/AllDogs/DogCard.tsx
+++ src/styled/AllDogs/DogCard.tsx
@@ -44,6 +44,7 @@ export const DogCard = styled(Col)`
   overflow: hidden;
   box-shadow: 0px 4px 4px 0px rgba(0, 0, 0, 0.25);
   color: white;
+  cursor: pointer;
 
   @media screen and (${devices.tablet}) {
     width: 30%;
