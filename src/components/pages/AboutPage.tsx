59b6ab0a3ef9f0e5bfaf58391a0a4c117b9ff9b4
petraelgemyr <petra.elgemyr@gmail.com>
2024-01-08 17:39:53 +0100

    hämta om föreningen-data

--- src/components/pages/AboutPage.tsx
+++ src/components/pages/AboutPage.tsx
@@ -1,7 +1,77 @@
+import { useState, useCallback, useEffect } from "react";
+import { useNavigate } from "react-router";
+import { useAppContext } from "../contexts/AppContext";
+import { PageName } from "../enums/PageName";
+import { filterPostsPerPage, findSlide } from "../helpers/FilterHelper";
+import { getAllPosts } from "../helpers/RepositoryHelper";
+import { IPost } from "../models/IPost";
+import { ISlide } from "../models/ISlide";
+import { ColCenteredButtonContainer } from "../../styled/Buttons/ColCenteredButtonContainer";
+import { SecondaryButton } from "../../styled/Buttons/SecondaryButton";
+import { ColCentered } from "../../styled/Common/Common";
+import { ColStart } from "../../styled/Spain/Spain";
+import { SlideCarousel } from "../SlideCarousel";
+
 export const AboutPage = () => {
+  const { posts, setPosts, slides } = useAppContext();
+  const [aboutPosts, setAboutPosts] = useState<IPost[]>([]);
+  const [aboutPageSlides, setAboutPageSlides] = useState<ISlide[]>([]);
+  const navigate = useNavigate();
+
+  const fetchPosts = useCallback(async () => {
+    // Fetch posts, filter them and set them to state
+    if (posts.length > 0) {
+      const filteredPosts = filterPostsPerPage(posts, PageName.About);
+      setAboutPosts(filteredPosts);
+    } else {
+      try {
+        const response = await getAllPosts();
+        if (response) {
+          setPosts(response);
+          const filteredPosts = filterPostsPerPage(response, PageName.About);
+          setAboutPosts(filteredPosts);
+        } else {
+          console.log("Inga inlägg");
+        }
+      } catch (error) {
+        console.error(error);
+      }
+    }
+  }, []);
+
+  useEffect(() => {
+    const aboutSlides: ISlide[] = [];
+    const slide = findSlide(slides, PageName.About);
+    if (slide) {
+      aboutSlides.push(slide);
+      setAboutPageSlides(aboutSlides);
+    }
+    fetchPosts();
+  }, [fetchPosts, slides]);
+
   return (
     <>
-      <h2>Om oss</h2>
+      <SlideCarousel slides={aboutPageSlides} />
+      <ColCentered>
+        <ColStart>
+          <h2>Om oss</h2>
+        </ColStart>
+        {aboutPosts.map((post, index) => {
+          return (
+            <ColStart key={index}>
+              <p style={{ fontFamily: "Korolev medium, sans-serif" }}>
+                {post.title}
+              </p>
+              <p>{post.postText}</p>
+            </ColStart>
+          );
+        })}
+        <ColCenteredButtonContainer>
+          <SecondaryButton onClick={() => navigate("/vart-arbetssatt")}>
+            Vårt arbetssätt
+          </SecondaryButton>
+        </ColCenteredButtonContainer>
+      </ColCentered>
     </>
   );
 };
