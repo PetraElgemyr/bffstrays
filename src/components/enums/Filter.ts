15c2f3a85d7e03e2e141abbf549fcf639eba4fd8
petraelgemyr <petra.elgemyr@gmail.com>
2024-01-09 23:16:33 +0100

    skapa enum för hållbarare kod

---
+++ src/components/enums/Filter.ts
@@ -0,0 +1,11 @@
+export enum Filter {
+  Female = "TIK",
+  Male = "HANE",
+  Small = "LITEN",
+  Medium = "MELLAN",
+  Big = "STOR",
+  Puppy = "VALP",
+  Adult = "VUXEN",
+  Senior = "SENIOR",
+  Clear = "RENSA",
+}
